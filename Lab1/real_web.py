b956f9591f4e23eebe61798b39658dde776463cb
Le Zhang <lezhangisu@gmail.com>
2018-09-27 12:57:46 -0500

    add progress bar to code

--- Lab1/real_web.py
+++ Lab1/real_web.py
@@ -1,4 +1,4 @@
-#
+# -*- coding: UTF-8 -*-
 
 from HTMLParser import HTMLParser
 import urllib2
@@ -13,6 +13,43 @@ class MyHTMLParser(HTMLParser):
         for attr in attrs:
             if 'href' == attr[0] and str(attr[1]).startswith("/wiki/"):
                 self.links.append(attr[1])
+
+class ShowProcess():
+    """
+    Progress bar class
+    """
+    i = 0 # current progress
+    max_steps = 0 # total steps
+    max_arrow = 50 # max length of the progress bar
+    infoDone = 'done'
+
+    # Initialization, takes in totoal steps and done info
+    def __init__(self, max_steps, infoDone = 'Done'):
+        self.max_steps = max_steps
+        self.i = 0
+        self.infoDone = infoDone
+
+    # show progress
+    # Effect: [>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>]100.00%
+    def show_process(self, i=None):
+        if i is not None:
+            self.i = i
+        else:
+            self.i += 1
+        num_arrow = int(self.i * self.max_arrow / self.max_steps) # calculate num of '>'
+        num_line = self.max_arrow - num_arrow # calculate num of '-'
+        percent = self.i * 100.0 / self.max_steps # calculate percentage, format: xx.xx%
+        process_bar = '[' + '>' * num_arrow + '-' * num_line + ']'\
+                      + '%.2f' % percent + '%' + '\r' # output string，'\r' means do not create new line
+        sys.stdout.write(process_bar) # print to screen
+        sys.stdout.flush()
+        if self.i >= self.max_steps:
+            self.close()
+
+    def close(self):
+        print('')
+        print(self.infoDone)
+        self.i = 0
 
 def getContent(url):
     html_page = urllib2.urlopen(url)
@@ -43,18 +80,34 @@ def bfs(ROOT_PAGE, INIT_PAGE, GOAL):
     num_visit = 0 # keep track of pages visited, start from 1 (first page)
     num_len = 0 # keep track of steps to reach the goal
 
+    cnt = 1
+    stage = 1
+    print "stage 1, " + str(cnt) + " in total"
+    process_bar = ShowProcess(cnt, 'Done')
     while queue:
+        if cnt > 0:
+            cnt -=1
+            process_bar.show_process()
+        else:
+            cnt = len(queue)-1
+            stage += 1
+            print ''
+            print "stage "+str(stage) + ", " + str(cnt) + " in total" 
+            process_bar = ShowProcess(cnt, 'Done')
+            process_bar.show_process()
+
+
         # get the item from queue
         page = queue.pop()
         num_visit+=1
         visited.append(page[0])
         content = getContent(ROOT_PAGE + page[0])
-        print page[0]
+        # print page[0]
         seq = list(page[1])
         if isGoalFound(content, GOAL):
             res_seq = list(seq)
             res_seq.append(page[0])
-
+            print ''
             # output the result sequence
             print "result ",res_seq
             # output the visited nodes and length of result path
@@ -73,6 +126,7 @@ def bfs(ROOT_PAGE, INIT_PAGE, GOAL):
 if __name__ == '__main__':
     ROOT_PAGE = 'https://en.wikipedia.org'
     INIT_PAGE = '/wiki/Association_football'
+    # INIT_PAGE = '/wiki/Brazil_national_football_team'
     # GOAL = 'Cristiano Ronaldo'
     GOAL = 'Ricardo Izecson dos Santos Leite'
     # GOAL = 'Pitch'
