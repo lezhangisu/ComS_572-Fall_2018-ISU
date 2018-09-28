f1c2326eaba93eb55fd8dd373b028bfaafad85ed
Le Zhang <lezhangisu@gmail.com>
2018-09-27 21:49:59 -0500

    switched to beam search, done

--- Lab1/real_web.py
+++ Lab1/real_web.py
@@ -7,13 +7,20 @@ import sys
 reload(sys)
 sys.setdefaultencoding("utf8")
 
+# initialize the reference map for score calculation
+score_dict = {}
 
+# parser class gets list of tuples: [(link, hypertext),...]
 class MyHTMLParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
-        for attr in attrs:
-            if 'href' == attr[0] and str(attr[1]).startswith("/wiki/"):
-                self.links.append(attr[1])
+        # print tag
+        i = 0
+        while i < len(attrs)-1:
+            if 'href' == attrs[i][0] and str(attrs[i][1]).startswith("/wiki/"):
+                self.links.append((attrs[i][1], attrs[i+1][1]))
+            i += 1
 
+# progress bar class
 class ShowProcess():
     """
     Progress bar class
@@ -51,12 +58,14 @@ class ShowProcess():
         print(self.infoDone)
         self.i = 0
 
+# get page content
 def getContent(url):
     html_page = urllib2.urlopen(url)
     text = html_page.read()
     html_page.close()
     return text
 
+# get href links out of html text
 def getHref(html_text):
     # html_page = getContent(url)
     parser = MyHTMLParser()
@@ -71,6 +80,8 @@ def isGoalFound(content, goal):
         return True
     return False
 
+# BFS to get path to the goal page
+# abandoned because of inefficiency
 def bfs(ROOT_PAGE, INIT_PAGE, GOAL):
     visited = [INIT_PAGE]
     # initialize the queue
@@ -113,22 +124,82 @@ def bfs(ROOT_PAGE, INIT_PAGE, GOAL):
             break
 
         for href in getHref(content):
-            if href in visited or href in [q[0] for q in queue]:
+            if href[0] in visited or href[0] in [q[0] for q in queue]:
                 continue
             temp_seq = list(seq)
             temp_seq.append(page[0])
             # add new ones to the queue
-            queue = [[href,temp_seq]] + queue
+            queue = [[href[0], temp_seq]] + queue
         time.sleep(2)
 
+
+#
+# Code for BEAM search
+#
+
+# score calculation for beam search
+def getScore(string):
+    score = 0
+    for s in score_dict.keys():
+        if s in string.lower():
+            score += score_dict[s]
+    return score
+
+## Beam search
+## its basically a best-first search with ranked list no longer than N
+## We pick N = 20 here
+def beam(ROOT_PAGE, INIT_PAGE, GOAL):
+    ranked_list = [(INIT_PAGE, 0, [])]
+    added_list = [INIT_PAGE]
+
+    visited_count = 0
+
+    while ranked_list:
+        # pop the one with highest score
+        current = ranked_list.pop()
+        content = getContent(ROOT_PAGE + current[0])
+        pg_score = content.count("QUERY")
+        visited_count += 1
+
+        if isGoalFound(content, GOAL):
+            # print "================"
+            ## output the sequence
+            print "Solution Path:\n", current[2]+[current[0]]
+            ## output the nodes visited and length of the answer
+            print 'Nodes visited:', visited_count
+            print 'Length of path: ', len(current[2])
+            # stop searching
+            break
+
+        for href in getHref(content):
+            if href[0] in added_list:
+                continue
+            added_list.append(href[0])
+            score = getScore(str(href[1]))
+            ranked_list.append((href[0], score, current[2]+[current[0]]))
+            added_list.append(href[0])
+
+        # sort the list by the score
+        # highest score at the end where will be popped up first
+        ranked_list = sorted(ranked_list, key=lambda x: x[1])
+        # keep items in the list less than 20 to save memory
+        ranked_list = ranked_list[-20:]
+
+# main function
 if __name__ == '__main__':
     ROOT_PAGE = 'https://en.wikipedia.org'
-    INIT_PAGE = '/wiki/Association_football'
-    # INIT_PAGE = '/wiki/Brazil_national_football_team'
-    # GOAL = 'Cristiano Ronaldo'
-    GOAL = 'Ricardo Izecson dos Santos Leite'
-    # GOAL = 'Pitch'
-    # print len(getHref("https://en.wikipedia.org/wiki/Association_football"))
-    # print getHref(ROOT_PAGE+INIT_PAGE)
-    bfs(ROOT_PAGE, INIT_PAGE, GOAL)
-    # getContent(ROOT_PAGE+INIT_PAGE)
+    INIT_PAGE = '/wiki/Sports'
+    GOAL = 'Paolo Cesare Maldini'
+    score_dict = {'soccer': 3, 'football': 3, 'paolo': 5, 'maldini': 5, 'serie a': 5,
+                    'italy': 4, 'milan': 5, 'world cup':5, 'association': 3}
+
+    ## another trial run
+    # INIT_PAGE = '/wiki/Main_Page'
+    # GOAL = 'Cristiano Ronaldo dos Santos Aveiro'
+    # score_dict = {'soccer': 3, 'football': 3, 'cristiano': 5, 'madrid': 5, 'la liga': 5,
+    #                 'portugal': 4, 'ronaldo': 5, 'world cup':5, 'association': 3}
+
+    print 'Start page: ' + ROOT_PAGE+INIT_PAGE
+    print 'Query words: ' + GOAL
+
+    beam(ROOT_PAGE, INIT_PAGE, GOAL)
